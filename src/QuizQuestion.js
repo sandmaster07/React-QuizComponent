7caa3f3c9474251ce82bced5169294718f0758e0
sandmaster07 <43596395+sandmaster07@users.noreply.github.com>
2019-05-09 10:45:34 +0800

    Create QuizQuestion.js

---
+++ src/QuizQuestion.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class QuizQuestion extends Component {
+  render() {
+    return (
+      <main>
+        <section>
+          <p>// instruction text goes here</p>
+        </section>
+        <section>
+          <ul>//quiz question button logic goes here</ul>
+        </section>
+      </main>
+    );
+  }
+}
+
+export default QuizQuestion;
